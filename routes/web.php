6c91794a42e1141ed21d7068cd8659b41e1890d7
Sami Mansour <samiman.net@gmail.com>
2024-12-14 12:27:50 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
-    Redis::incr('loading-page-views');
+    // Redis::incr('loading-page-views');
     return view('welcome');
 });
