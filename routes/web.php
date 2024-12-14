3e6c6c6766409def037f792beed315989257a6ba
Sami Mansour <samiman.net@gmail.com>
2024-12-14 12:11:13 +0300

    (Github) add redis

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
+    Redis::incr('loading-page-views');
     return view('welcome');
 });
