19003657056aa1452562d99bd46056cf064357b1
Sami Mansour <samiman.net@gmail.com>
2024-12-14 13:29:54 +0300

    wip

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,5 +3,7 @@
 it('returns a successful response', function () {
     $response = $this->get('/');
 
+    expect(\Illuminate\Support\Facades\Redis::get('loading-page-views'))->toEqual(1);
+
     $response->assertStatus(200);
 });
